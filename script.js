56ca162138c3aa79c3a15b4e9ae76f0881d8482c
Jay4G <143177827+Jay4G@users.noreply.github.com>
2026-04-21 17:13:45 +0100

    Honoured One Test

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 const creator = "Jay";
 console.log(`Hello ${creator}!`);
+
+document.getElementById("prompt-btn").addEventListener("click", function(){
+    let honouredOne = prompt("Who is the most honoured one?");
+
+    if (honouredOne) {
+        alert(`The most honoured one is ${honouredOne}!`);
+        console.log(`You are a true fan of ${honouredOne}!`);
+    } else {
+        console.log("Baaaaaakaaa!");
+    }
+});
