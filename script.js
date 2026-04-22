8dbf4c1ee26925b9c15269dc075c25b74f1577a4
Jay4G <143177827+Jay4G@users.noreply.github.com>
2026-04-22 09:54:41 +0100

    Gojo Satoru addition

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const creator = "Jay";
 console.log(`Hello ${creator}!`);
 
 document.getElementById("prompt-btn").addEventListener("click", function(){
-    let honouredOne = prompt("Who is the most honoured one?");
+    let honouredOne = prompt("Who is the most honoured one?", "Gojo Satoru");
 
     if (honouredOne) {
         alert(`The most honoured one is ${honouredOne}!`);
